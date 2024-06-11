d9dbc4f4280e13746761de08776ec53884abb5f6
Mohammad Fard <m.akramifard@gmail.com>
2024-06-11 13:51:38 +0330

    fix: deploy bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import "./App.css";
 import {TonConnectButton} from "@tonconnect/ui-react";
-import {Counter} from "./components/Counter";
-import {Jetton} from "./components/Jetton";
-import {TransferTon} from "./components/TransferTon";
 import styled from "styled-components";
-import {Button, FlexBoxCol, FlexBoxRow} from "./components/styled/styled";
+import {FlexBoxCol, FlexBoxRow} from "./components/styled/styled";
 import {useTonConnect} from "./hooks/useTonConnect";
-import {CHAIN} from "@tonconnect/protocol";
 import "@twa-dev/sdk";
 import {Game} from "./components/Game";
 
